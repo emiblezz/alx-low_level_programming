284fccd7e9104c779cb85824bcffee7f3039691d
ALEMI BLESS <alemibless@gmail.com>
2022-12-11 10:29:46 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void){
+	char alphabet='a';
+	while(alphabet<=z){
+		printf(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
