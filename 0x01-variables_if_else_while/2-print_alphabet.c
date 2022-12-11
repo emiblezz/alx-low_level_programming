d2b46acb079231b0b3139f6fc710e810d0d83184
alemi bless <107726040+emiblezz@users.noreply.github.com>
2022-12-11 21:33:59 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,11 @@
 #include <stdlib.h>
 
-#include <time.h>
-
 /* more headers goes there */
-
-
-
 /* betty style doc for function main goes there */
 
 int main(void){
 	char alphabet='a';
+	
 	while(alphabet<=z){
 		printf(alphabet);
 		alphabet++;
