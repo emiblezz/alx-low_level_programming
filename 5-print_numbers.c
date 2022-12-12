1b842f8f139901162c82fbb6509ca5c33a42a85c
alemi bless <107726040+emiblezz@users.noreply.github.com>
2022-12-13 02:46:00 +0300

    Create 5-print_numbers.c

---
+++ 5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<time.h>
+int main(void)
+{
+    int n=0;
+      
+    while(n<10)
+    {
+      printf("%i",n);
+      n++;
+    }
+      putchar("\n");
+  
+      return(0);
+}
