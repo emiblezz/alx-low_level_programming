03775252086b1cfce03990cda89977a16e5ee116
alemi bless <107726040+emiblezz@users.noreply.github.com>
2022-12-13 03:46:10 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-* Return: Always 0 (Success)
-*/
-int main(void){
-int nums;
-for(nums ='0';nums<='9';nums++){
-putchar(nums);}
-putchar('\n');
-return (0);
+ * main - print from 0 to 9 using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
 }
