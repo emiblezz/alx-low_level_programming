559e308617017055e4939b55f2d21c5ec3f3f532
alemi bless <107726040+emiblezz@users.noreply.github.com>
2022-12-15 05:54:22 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -16,3 +16,15 @@ void print_alphabet(void)
 	}
 		_putchar('\n');
 }
+#include "main.h"
+
+void print_alphabet(void)
+{
+	int i;
+	
+	for(i=97;i<123;i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
