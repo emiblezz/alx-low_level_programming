d7224df8caf4a41b9a27741a99b032df4aa80744
alemi bless <107726040+emiblezz@users.noreply.github.com>
2023-01-04 17:57:47 +0300

    Update 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - function that multiplies two integers
+* @a: First integer
+* @b: Second integer
+* Return: mul.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Se
