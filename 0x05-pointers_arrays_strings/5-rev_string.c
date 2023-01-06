b2841ef64bd9a19b40bc10746d8faeb35e601dc2
alemi bless <107726040+emiblezz@users.noreply.github.com>
2023-01-06 21:02:07 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,61 +1,31 @@
 #include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
- *
- *  *rev_string - update value.
- *
- *   *@s: value to be evaluate.
- *
- *    *Return: not.
- *
- *     */
-
-
+ *rev_string - update value.
+ *@s: value to be evaluate.
+ *Return: not.
+ */
 
 void rev_string(char *s)
-
 {
+	char tmp;
+	int i, len, len1;
 
-		char tmp;
+	len = 0;
+	len1 = 0;
 
-			int i, len, len1;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
+	len1 = len - 1;
 
-
-				len = 0;
-
-					len1 = 0;
-
-
-
-						while (s[len] != '\0')
-
-								{
-
-											len++;
-
-												}
-
-
-
-							len1 = len - 1;
-
-
-
-								for (i = 0; i < len / 2; i++)
-
-										{
-
-													tmp = s[i];
-
-															s[i] = s[len1];
-
-																	s[len1--] = tmp;
-
-																		}
-
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
